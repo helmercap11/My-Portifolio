33832eb5ee4f5706af3592d13dceec7c588512fe
Helmer Capassola <helmercap@gmail.com>
2019-03-02 14:26:28 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,418 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+<meta name="description" content="">
+<meta name="keywords" content="">
+<meta name="author" content="templatemo">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+<title>Helmer Capassola</title>
+<!--
+Stimulus Template
+http://www.templatemo.com/tm-498-stimulus
+-->
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/animate.css">
+<link rel="stylesheet" href="css/font-awesome.min.css">
+<link rel="stylesheet" href="css/templatemo-style.css">
+<link rel="icon" href="images/perfil1.jpg">
+
+<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700" rel="stylesheet">
+
+</head>
+<body data-spy="scroll" data-target=".navbar-collapse" data-offset="50">
+
+
+<!-- PRE LOADER -->
+
+<div class="preloader">
+     <div class="spinner">
+          <span class="spinner-rotate"></span>
+     </div>
+</div>
+
+
+<!-- Navigation Section -->
+
+<div class="navbar navbar-fixed-top custom-navbar" role="navigation">
+     <div class="container">
+
+          <!-- navbar header -->
+          <div class="navbar-header">
+               <button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="icon icon-bar"></span>
+                    <span class="icon icon-bar"></span>
+                    <span class="icon icon-bar"></span>
+               </button>
+               <a href="#" class="navbar-brand">Helmer Capassola</a>
+          </div>
+
+          <div class="collapse navbar-collapse">
+               <ul class="nav navbar-nav navbar-right">
+                    <li>
+                         <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" onClick="return confirm('De momento ainda não está disponivel')">
+                         <i data-count="Clique" class=" notification-icon" aria-label="Nofitication centre">Convite</i>
+                         </a>
+                    </li>
+                    <li><a href="#home" class="smoothScroll">Home</a></li>
+                    <li><a href="#about" class="smoothScroll">Sobre Me</a></li>
+                    <li><a href="#experience" class="smoothScroll">Habilidades</a></li>
+                    <li><a href="#quotes" class="smoothScroll">Interesses</a></li>
+                    <li><a href="#contact" class="smoothScroll">Contacto</a></li>
+               </ul>
+          </div>
+
+     </div>
+</div>
+
+
+<!-- Home Section -->
+
+<section id="home" class="parallax-section">
+     <div class="container">
+          <div class="row">
+
+               <div class="col-md-6 col-sm-6">
+                    <div class="home-img"></div>
+               </div>
+
+               <div class="col-md-6 col-sm-6">
+                    <div class="home-thumb">
+                         <div class="section-title">
+                              <h4 class="wow fadeInUp" data-wow-delay="0.3s">Bem Vindo ao meu website</h4>
+                              <h1 class="wow fadeInUp" data-wow-delay="0.6s">Olá, Eu Sou o <strong>Helmer</strong> Capassola.</h1>
+                              <p class="wow fadeInUp" data-wow-delay="0.9s">Sou desenvolvedor de sistema (Full - Stack) e fanático por tecnologia, gosto de desafios, e acima de tudo projectos que desafiam meus conhecimentos.</p>
+                              
+                              <a href="#about" class="wow fadeInUp smoothScroll section-btn btn btn-success" data-wow-delay="1.4s">Baixar</a>
+                              
+                         </div>
+                    </div>
+               </div>
+
+
+          </div>
+     </div>
+</section>
+
+
+<!-- About Section -->
+
+<section id="about" class="parallax-section">
+     <div class="container">
+          <div class="row">
+
+               <div class="col-md-6 col-sm-12">
+                    <div class="about-thumb">
+                         <div class="wow fadeInUp section-title" data-wow-delay="0.4s">
+                              <h1>Sobre</h1>
+                              <p class="color-yellow">Palavras de Motivação</p>
+                         </div>
+                         <div class="wow fadeInUp" data-wow-delay="0.8s">
+                              <p>Sempre fui apaixonado por aprender. Desde 2017 carrego o meu telemovel  para anotar tudo o que aprendo a cada dia. Acumulei vários, e só no final de 2018 resolvi reler o que eu tanto escrevia, e agora estou a por em prática.</p>
+                              <p>A melhor forma de aprender, a meu ver, é colocando a mão na massa e vivenciando experiências que nos mostram quais passos seguir. Se tiver um guia ao seu lado, melhor ainda. </p>
+                         </div>
+                    </div>
+               </div>
+
+               <div class="col-md-3 col-sm-6">
+                    <div class="background-image about-img"></div>
+               </div>
+
+               <div class="bg-yellow col-md-3 col-sm-6">
+                    <div class="skill-thumb">
+                         <div class="wow fadeInUp section-title color-white" data-wow-delay="1.2s">
+                              <h1>My Skills</h1>
+                              <p class="color-white"> HTML CSS JS. </p>
+                              <p class="color-white"> Web Android.</p>
+                         </div>
+
+                         <div class=" wow fadeInUp skills-thumb" data-wow-delay="1.6s">
+                         <strong>Frontend</strong>
+                              <span class="color-white pull-right">90%</span>
+                                   <div class="progress">
+                                        <div class="progress-bar progress-bar-primary" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100" style="width: 90%;"></div>
+                                   </div>
+
+                         <strong>Backend processing</strong>
+                              <span class="color-white pull-right">70%</span>
+                                   <div class="progress">
+                                        <div class="progress-bar progress-bar-primary" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width: 70%;"></div>
+                                   </div>
+
+                         <strong>HTML5 & CSS3</strong>
+                              <span class="color-white pull-right">80%</span>
+                                   <div class="progress">
+                                        <div class="progress-bar progress-bar-primary" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%;"></div>
+                                   </div>
+                         </div>
+
+                    </div>
+               </div>
+
+          </div>
+     </div>
+</section>
+
+
+<!-- Service Section -->
+
+<section id="service" class="parallax-section">
+     <div class="container">
+          <div class="row">
+
+               <div class="bg-yellow col-md-3 col-sm-6">
+                    <div class="wow fadeInUp color-white service-thumb" data-wow-delay="0.8s">
+                         <i class="fa fa-desktop"></i>
+                              <h3>D. Sistemas</h3>
+                              <p class="color-white">Analise, projeção, documentação, teste e manutenção de sistemas computacionais e software.</p>
+                    </div>
+               </div>
+
+               <div class="col-md-3 col-sm-6">
+                    <div class="wow fadeInUp color-white service-thumb" data-wow-delay="1.2s">
+                         <i class="fa fa-paper-plane"></i>
+                              <h3>Criação de WebSites</h3>
+                              <p class="color-white">Acredite, sem a criação de um site profissional sua empresa é invisível para milhares de pessoas.</p>
+                    </div>
+               </div>
+
+               <div class="bg-dark col-md-3 col-sm-6">
+                    <div class="wow fadeInUp color-white service-thumb" data-wow-delay="1.6s">
+                         <i class="fa fa-table"></i>
+                              <h3>Mobile App</h3>
+                              <p class="color-white">Seu aplicativo é sua ferramenta para melhor comunicação, facilitando, a compra, a marcação de consultas.</p>
+                    </div>
+               </div>
+
+               <div class="bg-white col-md-3 col-sm-6" >
+                    <div class="wow fadeInUp service-thumb" data-wow-delay="1.8s" style="margin-top: -30px;">
+                         <i class="fa fa-html5"></i>
+                              <h3>Código</h3>
+                              <p>Se seu site estiver com problemas de apresentação de palavras, como Can��o ou CanÃ§Ã£o ao invés de Canção.</p>
+                    </div>
+               </div>
+
+          </div>
+     </div>
+</section>
+
+
+<!-- Experience Section -->
+
+<section id="experience" class="parallax-section">
+     <div class="container">
+          <div class="row">
+
+               <div class="col-md-6 col-sm-6">
+                    <div class="background-image experience-img"></div>
+               </div>
+
+               <div class="col-md-6 col-sm-6">
+                    <div class="color-white experience-thumb">
+                         <div class="wow fadeInUp section-title" data-wow-delay="0.8s">
+                              <h1>Habilidades</h1>
+                              <!--<p class="color-white"></p>-->
+                         </div>
+
+                         <div class="wow fadeInUp color-white media" data-wow-delay="1.2s">
+                              <div class="media-object media-left">
+                                   <i class="fa fa-laptop"></i>
+                              </div>
+                              <div class="media-body">
+                                   <h3 class="media-heading">PHP</h3>
+                              </div>
+                              <div class="media-body">
+                                   <h3 class="media-heading">JAVA</h3>
+                              </div>
+                         </div>
+                         <div class="wow fadeInUp color-white media" data-wow-delay="1.6s">
+                              <div class="media-object media-left">
+                                   <i class="fa fa-laptop"></i>
+                              </div>
+                              <div class="media-body">
+                                   <h3 class="media-heading">Mysql</h3>
+                              </div>
+                              <div class="media-body">
+                                   <h3 class="media-heading">Android</h3>
+                              </div>
+                         </div>
+                         <div class="wow fadeInUp color-white media" data-wow-delay="1.2s">
+                              <div class="media-object media-left">
+                                   <i class="fa fa-laptop"></i>
+                              </div>
+                              <div class="media-body">
+                                   <h3 class="media-heading">HTML5 CSS3</h3>
+                              </div>
+                              <div class="media-body">
+                                   <h3 class="media-heading">JAVASCRIPT</h3>
+                              </div>
+                         </div>
+
+
+                    </div>
+               </div>
+
+          </div>
+     </div>
+</section>
+
+
+<!-- Education Section -->
+
+<section id="education" class="parallax-section">
+     <div class="container">
+          <div class="row">
+
+               <div class="col-md-6 col-sm-6">
+                    <div class="color-white education-thumb">
+                         <div class="wow fadeInUp section-title" data-wow-delay="0.8s">
+                              <h1>Formado</h1>
+                              <!--<p class="color-white"></p>-->
+                         </div>
+
+                         <div class="wow fadeInUp color-white media" data-wow-delay="1.2s">
+                              <div class="media-object media-left">
+                                   <i class="fa fa-laptop"></i>
+                              </div>
+                              <div class="media-body">
+                                   <h3 class="media-heading">Universidade Gregório Semedo<small> 2014 - DEZ 2017</small></h3>
+                                   <p class="color-white">ENGENHARIA INFORMÁTICA. Web Development Track. </p>
+                              </div>
+                         </div>
+
+                         <div class="wow fadeInUp color-white media" data-wow-delay="1.6s">
+                              <div class="media-object media-left">
+                                   <i class="fa fa-laptop"></i>
+                              </div>
+                              <div class="media-body">
+                                   <h3 class="media-heading">INSTITUTO MÉDIO INDUSTRIAL "SIMIONE MUCUNE<small> 2011 - DEZ 2013</small></h3>
+                                   <p class="color-white">ELETRONICA DE TELECOMUNICAÇÕES.</p>
+                              </div>
+                         </div>
+
+                    </div>
+               </div>
+
+               <div class="col-md-6 col-sm-6">
+                    <div class="background-image education-img"></div>
+               </div>
+
+          </div>
+     </div>
+</section>
+
+
+<!-- Quotes Section -->
+
+<section id="quotes" class="parallax-section">
+     <div class="overlay"></div>
+     <div class="container">
+          <div class="row">
+
+               <div class="col-md-offset-1 col-md-10 col-sm-12">
+                    <i class="wow fadeInUp fa fa-star" data-wow-delay="0.6s"></i>
+                    <h2 class="wow fadeInUp" data-wow-delay="0.8s">Além de ser um desenvolvedor web, eu aproveito parte do meu maior tempo ao ar livre.</h2>
+                    <p class="wow fadeInUp" data-wow-delay="1s">Gasto uma grande quantidade do meu tempo livre explorando os mais recentes avanços tecnológicos no mundo do desenvolvimento web de full stack.</p>
+               </div>
+
+          </div>
+     </div>
+</section>
+
+
+<!-- Contact Section -->
+
+<section id="contact" class="parallax-section">
+     <div class="container">
+          <div class="row">
+
+               <div class="col-md-6 col-sm-12">
+                    <div class="contact-form">
+                         <div class="wow fadeInUp section-title" data-wow-delay="0.2s">
+                              <h1 class="color-white">Olá ...</h1>
+                              <p class="color-white">Se você tem um projecto e precisa de alguém determinado, entre em contacto comigo, garanto o melhor preço e suporte, essa é a sua grande opurtunidade.</p>
+                         </div>
+
+                         <div id="contact-form">
+                              <form action="#" method="post">
+                                   <div class="wow fadeInUp" data-wow-delay="1s">
+                                        <input name="fullname" type="text" class="form-control" id="fullname" placeholder="Seu Nome">
+                                   </div>
+                                   <div class="wow fadeInUp" data-wow-delay="1.2s">
+                                        <input name="email" type="email" class="form-control" id="email" placeholder="Seu Email">
+                                   </div>
+                                   <div class="wow fadeInUp" data-wow-delay="1.4s">
+                                        <textarea name="message" rows="5" class="form-control" id="message" placeholder="Escreve a sua mensagem..."></textarea>
+                                   </div>
+                                   <div class="wow fadeInUp col-md-6 col-sm-8" data-wow-delay="1.6s">
+                                        <input name="submit" type="submit" class="form-control" id="submit" value="Enviar">
+                                   </div>
+                              </form>
+                         </div>
+
+                    </div>
+               </div>
+
+               <div class="col-md-3 col-sm-6">
+                    <div class="background-image contact-img"></div>
+               </div>
+
+               <div class="bg-dark col-md-3 col-sm-6">
+                    <div class="contact-thumb">
+                         <div class="wow fadeInUp contact-info" data-wow-delay="0.6s">
+                              <h3 class="color-white">Localização</h3>
+                              <p>Kilamba Kiaxi, Rua da Macom Luanda, Angola.</p>
+                         </div>
+
+                         <div class="wow fadeInUp contact-info" data-wow-delay="0.8s">
+                              <h3 class="color-white">Contacto.</h3>
+                              <p><i class="fa fa-phone"></i>(+244) 938-690-0390</p>
+                              <p><i class="fa fa-envelope-o"></i> <a href="helmercap@gmail.com">helmercap@gmail.com</a></p>
+                              <p><i class="fa fa-globe"></i> <a href="#">helmercap.co</a></p>
+                         </div>
+
+                    </div>
+               </div>
+
+          </div>
+     </div>
+</section>
+
+
+<!-- Footer Section -->
+
+<footer>
+	<div class="container">
+		<div class="row">
+
+               <div class="col-md-12 col-sm-12">
+                    <div class="wow fadeInUp footer-copyright" data-wow-delay="1.8s">
+                         <p>Copyright &copy; <script>document.write(new Date().getFullYear());</script> | <i class="fa fa" aria-hidden="true"></i>  <a href="https://facebook.com/helmerstarmeras?refid=12&ref=dbl" target="_blank">@helmercap11</a>
+                    
+                   </p>
+                    </div>
+				<ul class="wow fadeInUp social-icon" data-wow-delay="2s">
+                         <li><a href="https://facebook.com/helmerstarmeras?refid=12&ref=dbl" class="fa fa-facebook" target="_blank"></a></li>
+                         <li><a href="https://twitter.com/helmercap" class="fa fa-twitter" target="_blank"></a></li>
+                         <li><a href="https://github.com/helmercap11" class="fa fa-github" target="_blank"></a></li>
+                    </ul>
+               </div>
+			
+		</div>
+	</div>
+</footer>
+
+<!-- SCRIPTS -->
+
+<script src="js/jquery.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.parallax.js"></script>
+<script src="js/smoothscroll.js"></script>
+<script src="js/wow.min.js"></script>
+<script src="js/custom.js"></script>
+
+</body>
+</html>
